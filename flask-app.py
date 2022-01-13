58da957e5a2d3bed05b43f7fb11a11d23fa0539f
Jadamoureen <32454848+Jadamoureen@users.noreply.github.com>
2022-01-13 22:18:30 +0300

    Adding flask-app

---
+++ flask-app.py
@@ -0,0 +1,32 @@
+from calculateArces import acres 
+from flask import Flask, jsonify, request
+
+#instantiate flask object 
+    
+"""
+    A function to get request using flask
+    evaluate and return result
+    """
+      
+#app
+app = Flask('__name__')
+
+@app.route('/', methods=['GET', 'POST'])
+def get_input():
+    
+    packet = request.get_json(force=True)
+    print(packet)
+    length = packet['length']
+    width = packet['width']
+
+    acres_ = acres(length, width)
+
+    return {"Acres":acres_}
+
+#driver function 
+
+if __name__ == "__main__":
+  app.run(debug=True)
+    
+
+
